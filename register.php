a4cd7eab5342d39cfdb4eafd5cd02525e2b772ba
Uma-lang <155127507+Uma-lang@users.noreply.github.com>
2024-09-27 20:41:05 +0900

    Update register.php

--- register.php
+++ register.php
@@ -35,25 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $entry = $username . ',' . $hashed_password . PHP_EOL;
     file_put_contents($file, $entry, FILE_APPEND | LOCK_EX);
 
-    echo "登録成功！<a href='login.php'>ログインページへ</a>";
+    echo "登録成功！<a href='login.html'>ログインページへ</a>";
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>ユーザー登録</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <h2>ユーザー登録フォーム</h2>
-    <form action="register.php" method="post">
-        ユーザー名: <input type="text" name="username" required><br><br>
-        パスワード: <input type="password" name="password" required><br><br>
-        <input type="submit" value="登録">
-    </form>
-    <br>
-    <p>既にアカウントをお持ちの場合は、<a href="login.php">ログインページ</a>へ。</p>
-</body>
-</html>
